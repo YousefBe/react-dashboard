1bd93795ed621e3faad984f01dd39fec6c0239b7
YousefBe <joebelal7@gmail.com>
2023-04-07 18:14:58 +0200

    1H3M

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import './navbar.scss'
 function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search' />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon'/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className="counter">5</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import './Home.scss'
-import Sidebar from '../../components/sidebar/Sidebar'
+import React from "react";
+import "./Home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 
 function Home() {
   return (
-    <div className='home'>
-        <Sidebar/>
-        <div className="homeContainer">
-            container
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
